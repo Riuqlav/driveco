eaa5a2bc9d0a51c61cf70014b3d8c48d535b1b58
Riuqlav <valquirsolza.vs@gmail.com>
2024-03-05 12:49:12 +0100

    Populate the list with functions of API file

--- src/components/HubList/HubList.tsx
+++ src/components/HubList/HubList.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+// fetches data and populate the list.
+import React, { useState, useEffect } from 'react';
+import { fetchChargeBoxes } from '../../services/api';
+import HubItem from './HubItem';
+import { ChargeBox } from '../../utils/types';
 
 const HubList = () => {
-  return (
-    <div>HubList</div>
-  )
-}
+  const [chargeBoxes, setChargeBoxes] = useState<ChargeBox[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default HubList
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchChargeBoxes();
+        setChargeBoxes(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching charge boxes:', error);
+        setIsLoading(false); // Make sure to set isLoading to false in case of error
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleLoadMore = async () => {
+    // need to logic to load more charge boxes
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {chargeBoxes.map((box) => (
+            <HubItem key={box.identifier} box={box} />
+          ))}
+          <button onClick={handleLoadMore}>Load More</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default HubList;
