ff492e6a539a6d62e188adeed1958d2f903a1c64
Riuqlav <valquirsolza.vs@gmail.com>
2024-03-06 23:18:22 +0100

    Add functionality to distanceCalculator

--- src/utils/distanceCalculator.tsx
+++ src/utils/distanceCalculator.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import getDistance from 'geolib/es/getDistance';
+import { GeoLocation } from './types';
 
-export const distanceCalculator = () => {
-  return <div>distanceCalculator</div>;
+export const calculateDistance = (
+  userLocation: GeoLocation | null,
+  chargeBoxLocation: GeoLocation
+): string => {
+  if (!userLocation) {
+    return '0.00 km';
+  }
+
+  const distance = getDistance(
+    { latitude: userLocation.latitude, longitude: userLocation.longitude },
+    { latitude: chargeBoxLocation.latitude, longitude: chargeBoxLocation.longitude }
+  );
+
+  return `${(distance / 1000).toFixed(2)} km`;
 };
