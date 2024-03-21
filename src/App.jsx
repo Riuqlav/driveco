66c452bafb68a1f6a778e2fc2230e2f442f2a4cc
Riuqlav <valquirsolza.vs@gmail.com>
2024-03-21 15:45:54 +0100

    Set up file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import { useState, useEffect } from 'react';
-import BoxList from './components/HubList/BoxList';
 
 const App = () => {
-  const [userLocation, setUserLocation] = useState(null);
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const location = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        };
-        setUserLocation(location);
-        console.log('User location:', location);
-      },
-      (error) => {
-        console.error('Error getting location:', error);
-      }
-    );
-  }, []);
 
   return (
     <div>
-      <BoxList userLocation={userLocation} />
+      this is app speaking
     </div>
   );
 };
---
+++ src/components/ui/ChargeBoxItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChargeBoxItem = () => {
+  return (
+    <div>ChargeBoxItem</div>
+  )
+}
+
+export default ChargeBoxItem
