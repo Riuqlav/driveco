4ae8c693c2f488a1ff76c14495908bd50e2dac87
Riuqlav <valquirsolza.vs@gmail.com>
2024-03-29 11:52:49 +0100

    Add dynamic location to Gmaps button

--- src/components/ui/ChargeBoxItem.tsx
+++ src/components/ui/ChargeBoxItem.tsx
@@ -84,11 +84,13 @@ const ChargeBoxItem: React.FC<ChargeBoxItemProps> = ({
           </div>
         </div>
       </div>
-      {expanded && (
-        <div className="p-4 pt-0">
-          <ChargeBoxItemDetails type={type} address={address} />
-        </div>
-      )}
+      {
+  expanded && (
+    <div className="p-4 pt-0">
+      <ChargeBoxItemDetails type={type} address={address} location={location} />
+    </div>
+  )
+}
     </div>
   );
 };
--- src/components/ui/ChargeBoxItemDetails.tsx
+++ src/components/ui/ChargeBoxItemDetails.tsx
@@ -1,12 +1,32 @@
-// ChargeBoxItemDetails.jsx
 import React from 'react';
 
 interface ChargeBoxItemDetailsProps {
   type: string;
   address: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  } | null;
 }
 
-const ChargeBoxItemDetails: React.FC<ChargeBoxItemDetailsProps> = ({ type, address }) => {
+const ChargeBoxItemDetails: React.FC<ChargeBoxItemDetailsProps> = ({ type, address, location }) => {
+  const handleNavigateToGmaps = () => {
+    let gmapsUrl;
+
+    if (location) {
+      const { latitude, longitude } = location;
+      gmapsUrl = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+    } else {
+      gmapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`;
+    }
+
+    window.open(gmapsUrl, '_blank');
+  };
+
+  const handleBookChargingSession = () => {
+    window.open('https://driveco.com/', '_blank');
+  };
+
   return (
     <div className="flex flex-col">
       <p>
@@ -15,11 +35,19 @@ const ChargeBoxItemDetails: React.FC<ChargeBoxItemDetailsProps> = ({ type, addre
       <p>
         <strong>Address:</strong> {address}
       </p>
-      <div className="mt-2 flex">
-        <button className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <div className="mt-2 flex justify-between">
+        <button
+          className="flex items-center mr-2 bg-blue-200 hover:bg-blue-300 text-black py-2 px-4 rounded border border-black"
+          onClick={handleNavigateToGmaps}
+        >
+          <img src="src/assets/pointerBox.png" alt="Pointer" className="mr-2 h-4 w-4" />
           Navigate to Gmaps
         </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          className="flex items-center bg-green-200 hover:bg-green-300 text-black py-2 px-4 rounded border border-black"
+          onClick={handleBookChargingSession}
+        >
+          <img src="src/assets/key.png" alt="Key" className="mr-2 h-4 w-4" />
           Book a Charging Session
         </button>
       </div>
